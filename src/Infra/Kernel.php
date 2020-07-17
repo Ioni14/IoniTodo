f99433be4b29f3f5a02278431d10b4340d2be7ec
Thomas Talbot <thomas.talbot@sensiolabs.com>
2020-07-17 18:10:13 +0200

    fix kernel.php

--- src/Infra/Kernel.php
+++ src/Infra/Kernel.php
@@ -14,17 +14,19 @@ class Kernel extends BaseKernel
     protected function configureContainer(ContainerConfigurator $container): void
     {
         $container->import('../../config/packages/*.yaml');
-        $container->import('../../config/packages/'.$this->environment.'/*.yaml');
+        $container->import('../../config/packages/' . $this->environment . '/*.yaml');
 
         $container->import('../../config/services.yaml');
-        if (is_file('../../config/services_'.$this->environment.'.yaml')) {
-            $container->import('../../config/services_'.$this->environment.'.yaml');
+        if (is_file('../../config/services_' . $this->environment . '.yaml')) {
+            $container->import('../../config/services_' . $this->environment . '.yaml');
         }
     }
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../../config/routes/'.$this->environment.'/*.yaml');
+        if (is_file('../../config/routes/' . $this->environment . '/*.yaml')) {
+            $routes->import('../../config/routes/' . $this->environment . '/*.yaml');
+        }
         $routes->import('../../config/routes/*.yaml');
     }
 }
